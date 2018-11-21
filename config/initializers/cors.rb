8b9ee5e54472e5cd71a1d47ece8bb6a7a2263a32
learn <learn@learn08.localdomain>
2018-11-21 13:37:34 -0800

    updated origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://corgi-tinder.herokuapp.com/'
+    origins 'https://corgi-tinder.herokuapp.com'
 
     resource '*',
       headers: :any,
